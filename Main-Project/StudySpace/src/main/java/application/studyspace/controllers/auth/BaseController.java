3d3b82e09efe63e55d962df33556bfc2f64c3464
Spacerced <unger.cedric@yahoo.de>
2025-05-03 17:39:26 +0200

    tried to clean up style

--- Main-Project/StudySpace/src/main/java/application/studyspace/controllers/auth/BaseController.java
+++ Main-Project/StudySpace/src/main/java/application/studyspace/controllers/auth/BaseController.java
@@ -17,47 +17,42 @@ public class BaseController {
     public void initializeBase() {
         if (AnchorPaneLogin == null || LoginCard == null || Image01 == null) return;
 
-        // White card size bindings
+        // White login card size bindings
         LoginCard.prefWidthProperty().bind(AnchorPaneLogin.widthProperty().multiply(0.36));
         LoginCard.prefHeightProperty().bind(AnchorPaneLogin.heightProperty().multiply(0.6));
 
-        // Image size bindings
-        Image01.fitWidthProperty().bind(AnchorPaneLogin.widthProperty().multiply(0.28));
-        Image01.fitHeightProperty().bind(AnchorPaneLogin.heightProperty().multiply(0.5));
+        // Image scaling - larger, more central
+        Image01.fitWidthProperty().bind(AnchorPaneLogin.widthProperty().multiply(0.32));
+        Image01.fitHeightProperty().bind(AnchorPaneLogin.heightProperty().multiply(0.55));
 
-        // Position LoginCard (left 10%, vertically centered)
-        AnchorPaneLogin.widthProperty().addListener((obs, oldVal, newVal) -> {
-            AnchorPane.setLeftAnchor(LoginCard, newVal.doubleValue() * 0.10);
-        });
-        AnchorPaneLogin.heightProperty().addListener((obs, oldVal, newVal) -> {
-            AnchorPane.setTopAnchor(LoginCard, (newVal.doubleValue() - LoginCard.getHeight()) / 2);
-        });
+        // LoginCard position (left aligned, vertical center)
+        AnchorPaneLogin.widthProperty().addListener((obs, oldVal, newVal) ->
+                AnchorPane.setLeftAnchor(LoginCard, newVal.doubleValue() * 0.10));
+        AnchorPaneLogin.heightProperty().addListener((obs, oldVal, newVal) ->
+                AnchorPane.setTopAnchor(LoginCard, (newVal.doubleValue() - LoginCard.getHeight()) / 2));
 
-        // Position Image01 (right 6%, vertically centered)
-        AnchorPaneLogin.widthProperty().addListener((obs, oldVal, newVal) -> {
-            AnchorPane.setRightAnchor(Image01, newVal.doubleValue() * 0.06);
-        });
+        // Laptop image position (right aligned, vertically adjusted)
+        AnchorPaneLogin.widthProperty().addListener((obs, oldVal, newVal) ->
+                AnchorPane.setRightAnchor(Image01, newVal.doubleValue() * 0.06));
         AnchorPaneLogin.heightProperty().addListener((obs, oldVal, newVal) -> {
             double imageHeight = Image01.getFitHeight();
-            AnchorPane.setTopAnchor(Image01, (newVal.doubleValue() - imageHeight) / 2);
+            AnchorPane.setTopAnchor(Image01, (newVal.doubleValue() - imageHeight) / 2 - 10); // shift slightly up
         });
-        // Arc1 - widest and darkest
-        arc1.radiusXProperty().bind(AnchorPaneLogin.widthProperty().multiply(0.45));
+
+        // Arcs - fine tuned to match original visual proportions
+        arc1.radiusXProperty().bind(AnchorPaneLogin.widthProperty().multiply(0.40)); // slightly reduced
         arc1.radiusYProperty().bind(AnchorPaneLogin.heightProperty().multiply(0.5));
         arc1.centerXProperty().bind(AnchorPaneLogin.widthProperty());
         arc1.centerYProperty().bind(AnchorPaneLogin.heightProperty().divide(2));
 
-        // Arc2 - mid arc
-        arc2.radiusXProperty().bind(AnchorPaneLogin.widthProperty().multiply(0.35));
+        arc2.radiusXProperty().bind(AnchorPaneLogin.widthProperty().multiply(0.30)); // closer to original
         arc2.radiusYProperty().bind(AnchorPaneLogin.heightProperty().multiply(0.5));
         arc2.centerXProperty().bind(AnchorPaneLogin.widthProperty());
         arc2.centerYProperty().bind(AnchorPaneLogin.heightProperty().divide(2));
 
-        // Arc3 - innermost, lightest
-        arc3.radiusXProperty().bind(AnchorPaneLogin.widthProperty().multiply(0.25));
+        arc3.radiusXProperty().bind(AnchorPaneLogin.widthProperty().multiply(0.22)); // more visible
         arc3.radiusYProperty().bind(AnchorPaneLogin.heightProperty().multiply(0.5));
         arc3.centerXProperty().bind(AnchorPaneLogin.widthProperty());
         arc3.centerYProperty().bind(AnchorPaneLogin.heightProperty().divide(2));
-
     }
 }
