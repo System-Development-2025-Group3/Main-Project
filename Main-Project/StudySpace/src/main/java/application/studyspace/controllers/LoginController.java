d9af5b14bfe5b71a71838e2ceaec02217ce08152
Spacerced <unger.cedric@yahoo.de>
2025-05-03 14:41:39 +0200

    Refactored layout with VBox and controller cleanup

--- Main-Project/StudySpace/src/main/java/application/studyspace/controllers/LoginController.java
+++ Main-Project/StudySpace/src/main/java/application/studyspace/controllers/LoginController.java
@@ -24,82 +24,21 @@ import static application.studyspace.controllers.BaseController.LayoutBindings.b
 
 public class LoginController extends BaseController {
 
-    @FXML
-    private ImageView Image01;
-
-    @FXML
-    private AnchorPane AnchorPaneLogin;
-
-    @FXML
-    private Arc arc1, arc2, arc3;
-
-    @FXML
-    private TextFlow textFlow;
-
-    @FXML
-    private PasswordField InputPassword;
-
-    @FXML
-    private Button SubmitLoginButton;
-
-    @FXML
-    private Pane LoginPane;
-
-    @FXML
-    private CheckBox StayLoggedInCheckBox;
-
-    @FXML
-    private TextField DescriptionPasswordTextfield, DescriptionEmailTextfield, InputEmailTextfield;
-
-    @FXML
-    private Text linkText;
-
-    @FXML
-    private Label WelcomeTitle;
+    @FXML private ImageView Image01;
+    @FXML private AnchorPane AnchorPaneLogin;
+    @FXML private Arc arc1, arc2, arc3;
+    @FXML private TextFlow textFlow;
+    @FXML private PasswordField InputPassword;
+    @FXML private Button SubmitLoginButton;
+    @FXML private Pane LoginPane;
+    @FXML private CheckBox StayLoggedInCheckBox;
+    @FXML private TextField DescriptionPasswordTextfield, DescriptionEmailTextfield, InputEmailTextfield;
+    @FXML private Text linkText;
+    @FXML private Label WelcomeTitle;
 
     @FXML
     public void initialize() {
-        initializeBase();
-
-        //Contents of the Login Pane
-        double cumulativeHeight = 0.05; // Initial top margin (5% of LoginPane height)
-
-        // List of elements to configure
-        List<Region> elementsToConfigure = new ArrayList<>();
-        elementsToConfigure.add(WelcomeTitle);
-        elementsToConfigure.add(textFlow);
-        elementsToConfigure.add(DescriptionEmailTextfield);
-        elementsToConfigure.add(InputEmailTextfield);
-        elementsToConfigure.add(DescriptionPasswordTextfield);
-        elementsToConfigure.add(InputPassword);
-        elementsToConfigure.add(StayLoggedInCheckBox);
-
-        // Loop through the list and apply layout bindings
-        for (Region element : elementsToConfigure) {
-            cumulativeHeight = LayoutBindings.configureElementBindings(element, LoginPane, cumulativeHeight, 0.8, 0.1);
-        }
-
-        cumulativeHeight += 0.05;
-
-        //Button will be at 20% size instead of 80%
-        LayoutBindings.configureElementBindings(SubmitLoginButton, LoginPane, cumulativeHeight, 0.2, 0.1);
-
-        // Apply additional style bindings for specific elements if needed
-        Map<Region, Double> elementsWithFontScales = new LinkedHashMap<>();
-        elementsWithFontScales.put(WelcomeTitle, 0.05);
-        elementsWithFontScales.put(textFlow, 0.04);
-        elementsWithFontScales.put(DescriptionEmailTextfield, 0.04);
-        elementsWithFontScales.put(InputEmailTextfield, 0.04);
-        elementsWithFontScales.put(DescriptionPasswordTextfield, 0.04);
-        elementsWithFontScales.put(InputPassword, 0.04);
-        elementsWithFontScales.put(StayLoggedInCheckBox, 0.04);
-        elementsWithFontScales.put(SubmitLoginButton, 0.05);
-
-        // Loop through the map and apply the font size binding
-        elementsWithFontScales.forEach((region, fontScale) -> {
-            bindFontSize(region, LoginPane, fontScale); // Call utility method
-        });
-
+        initializeBase();  // Still handles arcs + image
         linkText.setOnMouseClicked(this::handleRegisterTextClick);
     }
 
